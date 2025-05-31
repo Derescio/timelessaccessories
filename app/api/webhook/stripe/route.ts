2288310d05ca4cb83b654e24b7b3285d1ac2e489
Derescio <ddw.web.dev.services@gmail.com>
2025-05-31 18:26:10 -0400

    updated apiVersion

--- app/api/webhook/stripe/route.ts
+++ app/api/webhook/stripe/route.ts
@@ -4,7 +4,7 @@ import { updateOrderToPaid } from '@/lib/actions/order.actions';
 import { sendOrderConfirmationEmail } from '@/email';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: '2023-10-16',
+  apiVersion: '2025-02-24.acacia',
 });
 
 export async function POST(req: NextRequest) {
